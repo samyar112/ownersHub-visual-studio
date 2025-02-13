382420c798268899ba4125448b026141bbd5b448
Samir Pandey <samee.pan1994@gmail.com>
2025-02-13 15:03:02 -0600

    pin generation in progress

--- src/app/login/new-user/new-user.component.ts
+++ src/app/login/new-user/new-user.component.ts
@@ -19,9 +19,9 @@ import { Login } from '../../model/login';
 export class NewUserComponent {
   readonly dialog = inject(MatDialog);
 
+  loginArray: Login[] = []
+  pin!: number;
   username!: string;
-  password!: string;
-
 
   constructor(
     private loginDataService: LoginDataService,
@@ -33,22 +33,58 @@ export class NewUserComponent {
       disableClose: true
     });
 
-    const pin = 1234
     const componentInstance = dialogRef.componentInstance as LoginCardComponent;
     if (componentInstance) {
       componentInstance.loginInfo.subscribe(async (loginData: any) => {
+        this.username = loginData.username;
         const allData = {
           username: loginData.username,
           password: loginData.password,
-          pin: pin
+          pin: this.pin
         };
         try {
           await this.loginDataService.addLoginData(allData);
           dialogRef.close();
+          this.openPinDialogBox()  
         } catch (error) {
           console.error('Login Upload Failed:', error)
         }
       });
     }
+  }
+
+  async getAllLoginInfo() {
+   this.loginArray = await this.loginDataService.getAllLoginData()
+  }
+
+  async openPinDialogBox() {
+
+    this.dialog.open(DialogBoxComponent, {
+      data: {
+        title: 'You are successfully registered',
+        content: `${this.username}, Save this PIN for future use. It will be necessary to access the tool on this machine.`,
+        username: this.username, // Include username
+        pin: this.pin, // Include generated PIN
+        closeButtonText: 'Close',
+        confirmButtonText: 'Confirm',
+      },
+      disableClose: true, // Prevent closing when clicking outside
+    });
+  }
+
+  async generateRandomPin(): Promise<number> {
+   
+
+    // Loop to generate a new pin until it is unique
+    do {
+      this.pin = Math.floor(1000 + Math.random() * 9000); // Generate a random 4-digit number
+
+      // Async check for uniqueness
+      // Wrapping the check in a Promise to simulate async behavior
+      await new Promise(resolve => setTimeout(resolve, 0)); // Simulate async behavior (this can be replaced with an actual async task)
+
+    } while (this.loginArray.some(user => user.pin === this.pin)); // Check if the generated PIN already exists
+
+    return this.pin;
   }
 }
